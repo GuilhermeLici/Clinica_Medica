7ea9b5ab948a73bbe1de58c63b87dc1c351896a5
unknown <you@example.com>
2023-10-06 17:23:27 -0300

    api filmes

---
+++ src/screens/Home/index.js
@@ -0,0 +1,78 @@
+
+import { StyleSheet, Text, View ,FlatList} from 'react-native';
+import BannerMovies from '../../components/bannerFilmes';
+import CardMovies from '../../components/cardFilmes';
+import Header from '../../components/header';
+import SearchBar from '../../components/searchbar';
+import Filmes from '../../data/movies';
+import React,{useState,useEffect} from 'react'
+
+
+export default function App() {
+  
+  const [movies,setMovies] = useState([]);
+  
+  useEffect(()=>{
+
+    async function buscarFilmes(){
+
+      const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=4e2c87fb914b204e70f8321de39a8426&language=pt-BR')
+      const data = response.json();
+      console.log(data.results);
+      setMovies(data.results)
+    
+    
+    }
+
+    buscarFilmes();
+
+
+
+
+
+
+
+  },[])
+
+  return (
+    <View style={styles.container}>
+     <Header></Header>
+
+     <SearchBar></SearchBar>
+
+     <BannerMovies></BannerMovies>
+     
+    
+     <View style = {{width :"90%"}}>
+          <FlatList
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            data={Filmes}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              
+              <CardMovies
+                titulo={item.nome}
+                imagem={item.imagem}
+                nota={item.nota}
+              />
+            )}
+          />
+        </View>
+    
+  
+
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#141a29',
+    alignItems:'center'
+    
+    
+  },
+});
